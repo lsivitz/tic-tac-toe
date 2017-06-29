bf93136d31373eaf7125a6da738a8bd2a3f6cb26
Lumen Sivitz <lumen@Orange.local>
2017-06-28 22:04:32 -0700

    fixed typo

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ class Board extends React.Component {
 
     handleClick(i) {
         const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares(i) {
+        if (calculateWinner(squares) || squares[i]) {
             return;
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
